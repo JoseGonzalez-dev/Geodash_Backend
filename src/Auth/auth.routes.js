d4112a8031b107e8a4b239c05603aa3bc7dd8074
José González <jgonzalez.242720@gmail.com>
2025-08-25 17:15:33 -0600

    Se eliminan archivos inecesarios

--- src/Auth/auth.routes.js
+++ src/Auth/auth.routes.js
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import { login, register } from "./auth.controller.js";
-import { uploadProfilePicture } from "../../middlewares/multer.upload.js";
 import { userValidator } from "../../middlewares/validators.js";
-import { deleteFileOnError } from "../../middlewares/delete.file.on.errors.js";
 
 const api = Router()
 
-api.post('/register',[uploadProfilePicture.single('profilePicture'),userValidator,deleteFileOnError],register)
+api.post('/register',[userValidator],register)
 api.post('/login',login)
 
 
