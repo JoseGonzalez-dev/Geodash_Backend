f794325874138ff0140f99c9df113ab7ec5b2197
rmartin-2023205 <rmartin-2023205@kinal.edu.gt>
2025-08-25 16:17:24 -0600

    Se agrega el modelo, controlador y rutas de la entidad Questions

---
+++ src/Questions/question.model.js
@@ -0,0 +1,21 @@
+import { Schema, model } from 'mongoose'
+
+const questionSchema = Schema({
+    category: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Category',
+        required: [true, 'Category is required']
+    }],
+    question: {
+        type: String,
+        required: [true, 'Question is required']
+    },
+    answer: {
+        type: String,
+        required: [true, 'Answer is required']
+    }
+},
+    { versionKey: false }
+)
+
+export default model('Question', questionSchema)
